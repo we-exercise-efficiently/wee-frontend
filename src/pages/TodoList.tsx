9ad021e0da5113fc4a7a7d08f27d6a4ea714f7d2
채진 정 <jeong9175@gmail.com>
2024-01-17 19:24:29 +0900

    Feat : Container 적용

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,5 +1,16 @@
+import Container from '../components/Container';
+
 export default function TodoList() {
   return (
-    <h2 className="font-bold text-red-700 text-3xl">TODO-LIST!</h2>
+    <>
+      <Container>
+        <h2 className="font-bold text-red-700 text-3xl">TODO-LIST!</h2>
+        {
+          <div className="font-thin text-blue-700 text-1xl">
+            <p>text</p>
+          </div>
+        }
+      </Container>
+    </>
   );
 }
