1234eafd7757012ff0f9fd47d3fd8d6abeef7067
shinji530 <shinji53059@gmail.com>
2024-01-17 21:16:46 +0900

    Feat: community 수정

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,5 +1,9 @@
+import Container from '../components/Container';
+
 export default function Community() {
   return (
-    <h2 className="font-bold text-red-700 text-3xl">COMMUNITY!</h2>
+    <Container>
+      <h2 className="font-bold text-red-700 text-3xl">COMMUNITY</h2>
+    </Container>
   );
 }
