242beace750da07161054c4ba7d19401e066b356
shinji530 <shinji53059@gmail.com>
2024-01-17 18:40:15 +0900

    Feat: feature/community branch 생성

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,3 +1,5 @@
 export default function Community() {
-  return <h2 className="font-bold text-red-700 text-3xl">COMMUNITY</h2>;
+  return (
+    <h2 className="font-bold text-red-700 text-3xl">COMMUNITY!</h2>
+  );
 }
