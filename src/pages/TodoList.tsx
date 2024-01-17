966d2d0af347997577be8fd1c7a22fcbbbeeb3d4
채진 정 <jeong9175@gmail.com>
2024-01-17 18:29:27 +0900

    Feat : todolist  브랜치 생성

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,3 +1,5 @@
 export default function TodoList() {
-  return <h2 className="font-bold text-red-700 text-3xl">TODO-LIST</h2>;
+  return (
+    <h2 className="font-bold text-red-700 text-3xl">TODO-LIST!</h2>
+  );
 }
